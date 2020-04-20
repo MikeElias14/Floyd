ffdac8280f15810cf888dea9928e5d56546422e3
MikeElias14 <michael@site469.com>
2020-04-20 12:29:45 -0400

    add pie chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartModule } from 'angular-highcharts';
+import { PieComponent } from './pie/pie.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
